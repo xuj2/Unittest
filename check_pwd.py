a8b976079633682ab3b5a2c540014cba7d50d709
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-05-10 17:55:27 -0700

    Change check_pwd to loop through the string, return false if no lower case letter found

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,10 @@
 def check_pwd(st):
-	if len(st) > 20 or len(st) < 8:
-		return False
-	return True
+        l_flag = 0
+        if len(st) > 20 or len(st) < 8:
+                return False
+        for letter in st:
+                if letter.islower():
+                        l_flag = 1
+        if l_flag == 0:
+                return False
+        return True
