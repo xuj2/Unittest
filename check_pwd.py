69527b9f245eac7a0ab80236b3941a2e9c32fdfa
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-05-10 18:02:44 -0700

    Change check_pwd to loop through the string, return false if no digit found

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,7 @@
 def check_pwd(st):
         l_flag = 0
         u_flag = 0
+        d_flag = 0
         if len(st) > 20 or len(st) < 8:
                 return False
         for letter in st:
@@ -8,6 +9,8 @@ def check_pwd(st):
                         l_flag = 1
                 if letter.isupper():
                         u_flag = 1
-        if l_flag == 0 or u_flag == 0:
+                if letter.isdigit():
+                        d_flag = 1
+        if l_flag == 0 or u_flag == 0 or d_flag == 0:
                 return False
         return True
