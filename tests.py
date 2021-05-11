b30443eeefeeacab3110e8fe7e6ceba0ea985e2b
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-05-10 17:39:53 -0700

    Add test2 to see if it accepts passwords with less than 8 characters

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
 	def test1(self):
 		self.assertFalse("1234567891012345678910")
 
+	def test2(self):
+		self.assertFalse("1234567")
+
 
 if __name__ == '__main__':
 	unittest.main()
