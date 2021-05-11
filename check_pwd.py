5725968fe5b8e3bc7b8e35c7c01068c3509502a1
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-05-10 17:58:52 -0700

    Change check_pwd to loop through the string, return false if no upper case letter found

--- check_pwd.py
+++ check_pwd.py
@@ -1,10 +1,13 @@
 def check_pwd(st):
         l_flag = 0
+        u_flag = 0
         if len(st) > 20 or len(st) < 8:
                 return False
         for letter in st:
                 if letter.islower():
                         l_flag = 1
-        if l_flag == 0:
+                if letter.isupper():
+                        u_flag = 1
+        if l_flag == 0 or u_flag == 0:
                 return False
         return True
