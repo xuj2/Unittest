8022a6fd3cfee84e217a570bafa6d5a152ba0950
xuj2 <72422668+xuj2@users.noreply.github.com>
2021-05-10 18:08:03 -0700

    Change check_pwd to loop through the string, return false if no symbol found

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,8 @@ def check_pwd(st):
         l_flag = 0
         u_flag = 0
         d_flag = 0
+        s_flag = 0
+        symbols = "~`!@#$%^&*()_+-="
         if len(st) > 20 or len(st) < 8:
                 return False
         for letter in st:
@@ -11,6 +13,8 @@ def check_pwd(st):
                         u_flag = 1
                 if letter.isdigit():
                         d_flag = 1
-        if l_flag == 0 or u_flag == 0 or d_flag == 0:
+                if letter in symbols:
+                        s_flag = 1
+        if l_flag == 0 or u_flag == 0 or d_flag == 0 or s_flag == 0:
                 return False
         return True
